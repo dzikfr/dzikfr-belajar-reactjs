b07b195f4fa4fee0e4a2b961a314cea97047d7d5
Dzikri Fauzi Ramdhani <dzikrifauziramdhani16@gmail.com>
2024-08-02 13:46:33 +0700

    02-08-2024

---
+++ src/materi/komponen/pembahasan/FunctionalComponent.jsx
@@ -0,0 +1,10 @@
+const FunctionalComponent = () =>{
+    return(
+        <div>
+        <h1>ini menggunakan functional component</h1>
+        <h2>ini juga menggunakan functional component</h2>        
+        </div>
+    )
+}
+
+export default FunctionalComponent;
