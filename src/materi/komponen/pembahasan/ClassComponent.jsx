c5e557e44687d01426634bfce221d53c72bf9692
Dzikri Fauzi Ramdhani <dzikrifauziramdhani16@gmail.com>
2024-08-02 12:57:52 +0700

    01-08-2024

---
+++ src/materi/komponen/pembahasan/ClassComponent.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class ClassComponent extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Hello World</h1>
+                <h3>Dzikri Fauzi Ramdhani</h3>
+            </div>
+        )
+    }
+}
+
+export default ClassComponent;
