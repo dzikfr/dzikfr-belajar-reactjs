1f4c83bf86466e85ea2149933d935507145d96e3
Dzikri Fauzi Ramdhani <dzikrifauziramdhani16@gmail.com>
2024-08-06 05:36:54 +0700

    06-08-2024/1

--- src/materi/komponen/pembahasan/ClassComponent.jsx
+++ src/materi/komponen/pembahasan/ClassComponent.jsx
@@ -1,6 +1,18 @@
 import React from "react";
 
 class ClassComponent extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Hello World</h1>
+                <h3>Hallo {this.props.nama}</h3>
+                <button onClick={this.handleMin}>-</button>
+                <span>{'  '}{this.state.value}{'  '}</span>
+                <button onClick={this.handlePlus}>+</button>
+            </div>
+        )
+    }
+
     constructor(props){
         super(props)
 
@@ -15,20 +27,9 @@ class ClassComponent extends React.Component{
     handlePlus(){
         this.setState({value: this.state.value +1})
     }
+    
     handleMin(){
         this.setState({value: this.state.value -1})
-    }
-
-    render(){
-        return(
-            <div>
-                <h1>Hello World</h1>
-                <h3>Hallo {this.props.nama}</h3>
-                <button onClick={this.handleMin}>-</button>
-                <span>{'  '}{this.state.value}{'  '}</span>
-                <button onClick={this.handlePlus}>+</button>
-            </div>
-        )
     }
 }
 
--- src/materi/komponen/pembahasan/FunctionalComponent.jsx
+++ src/materi/komponen/pembahasan/FunctionalComponent.jsx
@@ -1,9 +1,9 @@
-const FunctionalComponent = ({nama}) =>{
+const FunctionalComponent = (props) =>{ //({nama})
     return(
         <div>
             <h1>ini menggunakan functional component</h1>
             <h2>ini juga menggunakan functional component</h2>   
-            <h2>Hallo {nama}</h2>     
+            <h2>Hallo {props.nama}</h2> {/*Hallo {nama}*/}
         </div>
     )
 }
